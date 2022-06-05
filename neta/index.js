f339adab9feddc15c171e27bb4efb38e6d503ab8
BuildTools <unconfigured@null.spigotmc.org>
2022-06-05 22:12:40 +0900

    test

---
+++ neta/index.js
@@ -0,0 +1,28 @@
+
+window.AudioContext = window.AudioContext || window.webkitAudioContext;
+
+// AudioContextを生成
+var ctx = new AudioContext();
+
+// 指定した音源ファイルをバイナリデータとして取得
+var xml = new XMLHttpRequest();
+xml.responseType = 'arraybuffer';
+xml.open('GET', 'sample.wav', true);
+xml.onload = function() {
+    // 取得したバイナリデータをデコード
+    ctx.decodeAudioData(
+        xml.response,
+        function(_data) {
+            data = _data;
+        },
+        function(err) {
+            alert(e.err);
+        }
+    );
+};
+xml.send();
+bufferSource.buffer = data;
+// 再生速度を2倍に設定すると、周波数も2倍になり、音は高くなる
+bufferSource.playbackRate.value = 2;
+bufferSource.connect(ctx.destination);
+bufferSource.start(0);
---
+++ src/js/watch.js
@@ -0,0 +1,10 @@
+window.onload = function() {
+    var p = (new URL(document.location)).searchParams;
+    v = p.get('v');
+    console.log(v)
+    var d = function() {
+        
+    }
+    document.title = "動画タイトル"
+}
+
